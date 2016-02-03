8e5d499de8f273044735ecbbeb03f9000ad5115a
Kenny <ken.chad.howard@gmail.com>
2016-02-03 14:06:11 -0700

    adding in jquery-bootstrap and creating gulp task for bundle.css

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var open = require('gulp-open'); //Open a URL in a web browser
 var browserify = require('browserify'); //Bundles JS
 var reactify = require('reactify'); //Transforms React JSX to JS
 var source = require('vinyl-source-stream'); //Use conventional text streams with Gulp
+//CSS task to handle our bootstrap work
+var concat = require('gulp-concat'); //Concatenates files
 
 var config = {
   port: 9876,
@@ -14,6 +16,10 @@ var config = {
   paths: {
     html: './src/*.html', //Find anything that ends in html
     js: './src/**/*.js',
+    css: [
+      'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+    ],
     dist: './dist',
     mainJs: './src/main.js'
   }
@@ -53,10 +59,17 @@ gulp.task('js', function() {
     .pipe(connect.reload()); //Reloads the app with JS changes, seeing the latest version in the browser
 });
 
+//Need a CSS task - Gulp Concat
+gulp.task('css', function() {
+  gulp.src(config.paths.css)
+    .pipe(concat('bundle.css'))
+    .pipe(gulp.dest(config.paths.dist + '/css')); //Same as before but for css
+});
+
 //Need a task to watch files and do a live reload
 gulp.task('watch', function() {
   gulp.watch(config.paths.html, ['html']);
 });
 
 //Need a default task
-gulp.task('default', ['html', 'js', 'open', 'watch']); //Now I can run these items, when I type in Gulp on he command line
+gulp.task('default', ['html', 'js', 'css', 'open', 'watch']); //Now I can run these items, when I type in Gulp on he command line
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+$ = jQuery = require('jquery'); //Referencing jQuery by 'jQuery' or $
+
 var App = console.log('Hello world from Browserify');
 
 module.exports = App;
